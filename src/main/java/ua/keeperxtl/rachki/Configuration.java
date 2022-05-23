5b6a784cdd1be4d3edfa05367eb9049ec42cec24
Sergey Mnatsakanian <sergosergo0001@gmail.com>
2022-05-23 03:06:04 +0300

    add mobile version

---
+++ src/main/java/ua/keeperxtl/rachki/Configuration.java
@@ -0,0 +1,32 @@
+package ua.keeperxtl.rachki;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration implements WebMvcConfigurer {
+    @Bean
+    public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
+        return new DeviceResolverHandlerInterceptor();
+    }
+
+    @Bean
+    public DeviceHandlerMethodArgumentResolver deviceHandlerMethodArgumentResolver() {
+        return new DeviceHandlerMethodArgumentResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(deviceResolverHandlerInterceptor());
+    }
+
+    @Override
+    public void addArgumentResolvers(List argumentResolvers) {
+        argumentResolvers.add(deviceHandlerMethodArgumentResolver());
+    }
+}
--- src/main/java/ua/keeperxtl/rachki/MainController.java
+++ src/main/java/ua/keeperxtl/rachki/MainController.java
@@ -1,12 +1,35 @@
 package ua.keeperxtl.rachki;
 
+import org.springframework.mobile.device.Device;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    public String homePage() {
-        return "index";
+    @GetMapping()
+    public String homePage(Device device) {
+
+        String deviceType = "browser";
+        String platform = "browser";
+        String viewName = "index";
+
+        if (device.isNormal()) {
+            deviceType = "browser";
+        } else if (device.isMobile()) {
+            deviceType = "mobile";
+            viewName = "mobile/index";
+        } else if (device.isTablet()) {
+            viewName = "index";
+        }
+
+        platform = device.getDevicePlatform().name();
+
+        if (platform.equalsIgnoreCase("UNKNOWN")) {
+            platform = "browser";
+        }
+
+        return viewName;
     }
 
 }
